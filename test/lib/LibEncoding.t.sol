de671a94a13053203521b58ad3ebcde98e795891
kadenzipfel <kaden.zipfel@hotmail.com>
2023-05-26 13:55:10 -0700

    Prepare LibEncoding_Test

---
+++ test/lib/LibEncoding.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { Test } from "forge-std/Test.sol";
+import { TestUtils } from "test/testutils/TestUtils.sol";
+import { TestArithmetic } from "test/testutils/TestArithmetic.sol";
+import { LibEncoding } from "src/lib/LibEncoding.sol";
+import "src/types/Types.sol";
+
+/// @title LibEncoding_Test
+/// @notice Tests for the `LibEncoding` library.
+contract LibEncoding_Test is Test {
+    
+}
