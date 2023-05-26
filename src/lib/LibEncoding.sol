5533fa5c322f85b94d25695bffcc30f3ba64dde2
kadenzipfel <kaden.zipfel@hotmail.com>
2023-05-26 13:49:30 -0700

    Prepare LibEncoding

---
+++ src/lib/LibEncoding.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { RLPWriterLib } from "./rlp/RLPWriterLib.sol";
+
+/// @title LibEncoding
+/// @notice A library for Optimism's various different encoding schemes.
+library Encoding {
+
+}
